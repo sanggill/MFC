23c906bf9dba29309f9dcb4f896ad66db9a9a0a9
SanggilYOU <149997952+sanggill@users.noreply.github.com>
2023-11-17 16:05:45 +0900

    231117-1

---
+++ socServerDlg.h
@@ -0,0 +1,46 @@
+﻿
+// socServerDlg.h: 헤더 파일
+//
+
+#pragma once
+
+#include "CServerSocket.h"
+// CsocServerDlg 대화 상자
+class CsocServerDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CsocServerDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SOCSERVER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+private:
+	CClientSocket* m_pClientSocket;
+	CServerSocket* m_pServerSocket;
+
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CListBox m_list_client;
+	CListBox m_list_msg;
+	CPtrList m_ptrClientSocketList;	//For manage Client Sockets
+protected:
+	afx_msg LRESULT OnAcceptSocket(WPARAM wParam, LPARAM lParam);
+	afx_msg LRESULT OnCleintMsgRecv(WPARAM wParam, LPARAM lParam);
+	afx_msg LRESULT OnClientClose(WPARAM wParam, LPARAM lParam);
+};
